a5e047819ac82eeb8167f7105f8d498ae27a3efc
KaitoMuraoka <kaitoboshi@icloud.com>
2023-11-16 09:59:08 +0900

    エントリーを変更
    - .library -> .executable
    - .target -> .executableTarget

--- Package.swift
+++ Package.swift
@@ -7,14 +7,14 @@ let package = Package(
     name: "importformatter",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
+        .executable(
             name: "importformatter",
             targets: ["importformatter"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
+        .executableTarget(
             name: "importformatter"),
         .testTarget(
             name: "importformatterTests",
